9a0db0d13e4fe2d50a103d1d3bb6cebafa5d0a6e
Perry John Kwarteng <perrykwarteng8@gmail.com>
2025-02-11 16:44:36 +0000

    adding primng to the project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,14 +4,20 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { dashboardRoutes } from './Pages/dashboard/dashboard.routing';
-import { provideImageKitLoader } from '@angular/common';
 import { provideHttpClient } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideRouter(dashboardRoutes),
     provideHttpClient(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
     provideAnimations(),
   ],
 };
