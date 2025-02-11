93d9e9f9f740fddf6fde65863f7c7f0cdc6ac0eb
Perry John Kwarteng <perrykwarteng8@gmail.com>
2025-02-11 16:51:56 +0000

    reusable btn component

--- src/app/Shared/components/button-icon/button-icon.component.ts
+++ src/app/Shared/components/button-icon/button-icon.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'app-button-icon',
-    imports: [],
-    templateUrl: './button-icon.component.html',
-    styleUrl: './button-icon.component.css'
+  selector: 'app-button-icon',
+  imports: [],
+  templateUrl: './button-icon.component.html',
+  styleUrl: './button-icon.component.css',
 })
 export class ButtonIconComponent {
-
   @Input() icon: string = '';
   @Input() text: string = '';
+  @Input() bgStyles: string = '';
 }
