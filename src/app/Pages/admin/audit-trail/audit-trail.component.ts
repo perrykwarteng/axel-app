a4229cd63e57af429bbe8dc13c1be7227c13ada0
Perry John Kwarteng <perrykwarteng8@gmail.com>
2025-02-11 16:45:12 +0000

    created audit trail component

---
+++ src/app/Pages/admin/audit-trail/audit-trail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audit-trail',
+  imports: [],
+  templateUrl: './audit-trail.component.html',
+  styleUrl: './audit-trail.component.css'
+})
+export class AuditTrailComponent {
+
+}
